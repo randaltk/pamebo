11763e059237827c54bbaa7164494f157d6ed343
Randal <randal.t.krebs@gmail.com>
2024-05-24 01:20:32 -0300

    ajuste fino footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,13 @@
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
 import Link from "next/link";
-
+import "./footer.css";
 const Footer = () => {
   return (
-    <footer className="bg-blue-custom text-white p-8">
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-center gap-8">
-        <div className="flex flex-col md:flex-row md:ml-16 gap-8">
-          <div className="mb-4 md:mb-0">
+    <footer className="bg-blue-custom text-white p-8 ">
+      <div className="container mx-auto flex flex-col  md:flex-row items-center justify-center gap-8 ajusteFino">
+        <div className="flex flex-col md:flex-row md:ml-16 gap-8 ">
+          <div className="mb-4 md:mb-0 ">
             <h4 className="text-lg font-bold mb-2">Links</h4>
             <ul>
               <li>
@@ -35,7 +35,7 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className="mb-4 md:mb-0">
+          <div className="mb-4 md:mb-0 ajusteFino2" >
             <h4 className="text-lg font-bold mb-2">Redes Sociais</h4>
             <ul>
               <li>
@@ -75,14 +75,14 @@ const Footer = () => {
                 <a href="#">orcamentos@pamebopinturas.com.br</a>
               </li>
               <li>
-                <a href="#"> 5549-7175 ( São Paulo )</a>
+                <a href="#">5549-7175 ( São Paulo )</a>
               </li>
             </ul>
           </div>
         </div>
       </div>
 
-      <div className="mt-8 border-t border-gray-600 pt-6 text-center">
+      <div className="mt-8 border-t border-gray-600 pt-6 text-center ">
         <p>
           <AiOutlineMail className="inline-block mr-2" />{" "}
           orcamentos@pamebopinturas.com.br
