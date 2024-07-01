2a57ed1530f5c271a763f3006cbf6cc0c5e583ac
Randal <randal.t.krebs@gmail.com>
2024-07-01 20:24:36 -0300

    add contact form

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,170 +1,136 @@
-import PageBanner from '@/components/pagebanner/PageBanner';
-import Head from 'next/head';
+"use client"
+
+import PageBanner from "@/components/pagebanner/PageBanner";
+import Head from "next/head";
+import { useForm, ValidationError } from "@formspree/react";
 
 const Contato = () => {
+  const [state, handleSubmit] = useForm(`${process.env.FORM}`);
+
+  if (state.succeeded) {
+    return (
+      <>
+        <div className="relative">
+          <h2 className="text-4xl font-bold mb-4 text-center z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white text-black rounded-lg p-4 shadow-md">
+            Contato
+          </h2>
+          <PageBanner src="/pagebanners/3.png" alt="Contato Banner" />
+        </div>
+
+        <div className="container mx-auto mt-10">
+          <p className="text-center text-2xl text-green-600 mt-4">
+            Obrigado por entrar em contato!
+          </p>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <div className="relative">
-      <h2 className="text-4xl font-bold mb-4 text-center z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white text-black rounded-lg p-4 shadow-md">Contato</h2>
-
+        <h2 className="text-4xl font-bold mb-4 text-center z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white text-black rounded-lg p-4 shadow-md">
+          Contato
+        </h2>
         <PageBanner src="/pagebanners/3.png" alt="Contato Banner" />
       </div>
+
       <div className="container mx-auto mt-10">
         <Head>
           <title>Contato</title>
         </Head>
 
-
         <div className="p-6 flex-grow flex flex-col justify-between">
           <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div className="mt-8 overflow-hidden">
-              <div className="grid grid-cols-1 md:grid-cols-2">
+              <form
+                onSubmit={handleSubmit}
+                className="grid grid-cols-1 md:grid-cols-2 gap-6"
+              >
                 <div className="p-6 mr-2 bg-gray-100 dark:bg-gray-800 sm:rounded-lg">
                   <h1 className="text-4xl sm:text-5xl text-gray-800 dark:text-white font-extrabold tracking-tight">
                     Fale Conosco
                   </h1>
                   <p className="text-normal text-lg sm:text-2xl font-medium text-gray-600 dark:text-gray-400 mt-2">
                     Entre em contato ou deixe sua solicitação ao lado para ser
-                    atentido(a) horario comercial
+                    atendido(a) durante o horário comercial.
                   </p>
-
-                  <div className="flex items-center mt-4 text-gray-600 dark:text-gray-400">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      className="w-8 h-8 text-gray-500"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M12 6V12L15 15"
-                      />
-                      <circle cx={12} cy={12} r={10} />
-                    </svg>
-                    <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                      Horário segunda a sexta-feira 08:00 às 18:00 horas
-                    </div>
-                  </div>
-
-                  <div className="flex items-center mt-4 text-gray-600 dark:text-gray-400">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="1.5"
-                      viewBox="0 0 24 24"
-                      className="w-8 h-8 text-gray-500"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="1.5"
-                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                      />
-                    </svg>
-                    <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                      +55 (11) 2275-7514
-                    </div>
-                  </div>
-
-                  <div className="flex items-center mt-2 text-gray-600 dark:text-gray-400">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="1.5"
-                      viewBox="0 0 24 24"
-                      className="w-8 h-8 text-gray-500"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="1.5"
-                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                      />
-                    </svg>
-                    <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                      orcamentos@pamebopinturas.com.br
-                    </div>
-                  </div>
                 </div>
 
-                <form className="p-6 flex flex-col justify-center">
-                  <div className="flex flex-col">
-                    <label htmlFor="name" className="hidden">
-                      Nome
-                    </label>
-                    <input
-                      type="name"
-                      name="name"
-                      id="name"
-                      placeholder="Nome "
-                      className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <label htmlFor="name" className="hidden">
-                      Sobrenome
-                    </label>
-                    <input
-                      type="lastname"
-                      name="lastname"
-                      id="lastname"
-                      placeholder="Sobrenome"
-                      className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                    />
-                  </div>
-                  <div className="flex flex-col mt-2">
-                    <label htmlFor="email" className="hidden">
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="Email"
-                      className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                    />
-                  </div>
+                <div className="flex flex-col p-6">
+                  <label htmlFor="name" className="hidden">
+                    Nome
+                  </label>
+                  <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    placeholder="Nome"
+                    className="w-full mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
+                  />
 
-                  <div className="flex flex-col mt-2">
-                    <label htmlFor="tel" className="hidden">
-                      Numero de telefone
-                    </label>
-                    <input
-                      type="tel"
-                      name="tel"
-                      id="tel"
-                      placeholder="Telefone"
-                      className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                    />
-                  </div>
-                  <div className="flex flex-col mt-2">
-                    <label htmlFor="tel" className="hidden">
-                      Mensagem
-                    </label>
+                  <label htmlFor="lastname" className="hidden">
+                    Sobrenome
+                  </label>
+                  <input
+                    id="lastname"
+                    type="text"
+                    name="lastname"
+                    placeholder="Sobrenome"
+                    className="w-full mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
+                  />
 
-                    <textarea
-                      id="Mensagem"
-                      placeholder="Mensagem"
-                      name="message"
-                      required
-                      className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                    />
-                  </div>
+                  <label htmlFor="tel" className="hidden">
+                    Telefone
+                  </label>
+                  <input
+                    id="tel"
+                    type="tel"
+                    name="tel"
+                    placeholder="Telefone"
+                    className="w-full mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
+                  />
+
+                  <label htmlFor="email" className="hidden">
+                    Email
+                  </label>
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="Seu Email"
+                    className="w-full mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
+                  />
+                  <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                  />
+
+                  <label htmlFor="message" className="hidden">
+                    Mensagem
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    placeholder="Sua Mensagem"
+                    className="w-full mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
+                  />
+                  <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                  />
+
                   <button
                     type="submit"
                     className="md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300"
+                    disabled={state.submitting}
                   >
                     Enviar
                   </button>
-                </form>
-              </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
